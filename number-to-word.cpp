47917be14f385b61cafa1d936c364fc974efb4f1
DanikingRD <danikingrd@gmail.com>
2023-11-13 09:20:58 -0400

    added user input and validation

--- number-to-word.cpp
+++ number-to-word.cpp
@@ -1,71 +1,83 @@
+#include <cmath>
 #include <iostream>
-#include <type_traits>
 
 using namespace std;
 
 // Realizar un programa C++ que lea un número real (correspondiente a un monto)
 // y convertir este monto a palabra.
 
-const float LIMIT = 999999999.99f;
+const float LIMIT = 999999999.99F;
 
-const string base[] = {"",      "uno",  "dos",   "tres", "cuatro",
-                       "cinco", "seis", "siete", "ocho", "nueve"};
-const string especiales[] = {
-    "diez",   "once",      "doce",       "trece",     "catorce",
-    "quince", "dieciseis", "diecisiete", "dieciocho", "diecinueve"};
-const string decenas[] = {"",         "",         "veinti",  "treinta",
-                          "cuarenta", "cicuenta", "sesenta", "setenta",
-                          "ochenta",  "noventa",  "cien"};
+const string base[] = {"", "uno", "dos", "tres", "cuatro", "cinco", "seis", "siete", "ocho", "nueve"};
+const string especiales[] = {"diez",   "once",      "doce",       "trece",     "catorce",
+                             "quince", "dieciseis", "diecisiete", "dieciocho", "diecinueve"};
+const string decenas[] = {"",        "",        "veinti",  "treinta", "cuarenta", "cicuenta",
+                          "sesenta", "setenta", "ochenta", "noventa", "cien"};
 
-const string centenas[] = {
-    "",           "ciento",      "doscientos",  "trescientos", "cuatrocientos",
-    "quinientos", "seiscientos", "setecientos", "ochocientos", "novecientos"};
+const string centenas[] = {"",           "ciento",      "doscientos",  "trescientos", "cuatrocientos",
+                           "quinientos", "seiscientos", "setecientos", "ochocientos", "novecientos"};
 
-int readInt() {
-  int n;
-  while (!(cin >> n)) {
-    printf("Ingrese un número entero válido: ");
-    cin.clear();
-    cin.ignore(123);
-  }
-  return n;
+string findNameForNumber(int n) {
+    if (n == 0)
+        return "cero";
+    if (n == 20)
+        return "veinte";
+    if (n == 100)
+        return "cien";
+    if (n == 1000)
+        return "mil";
+
+    if (n < 10) {
+        // cubre del 1 al 10
+        return base[n];
+    } else if (n < 20) {
+        // cubre del 10 al 19
+        return especiales[n - 10];
+    } else if (n < 100) {
+        // cubre del 20 al 99
+        string separador = (n % 10 == 0 || n < 30) ? "" : " y ";
+        return decenas[n / 10] + separador + base[n % 10];
+    } else if (n < 1000) {
+        // cubre del 100 al 999
+        string decena = (n % 100 == 0 ? "" : findNameForNumber(n % 100));
+        return centenas[n / 100] + ' ' + decena;
+    } else if (n < LIMIT) {
+        string unidad = (n % 1000 == 0 ? "" : findNameForNumber(n % 1000));
+        // para los numeros entre 1000 y 1999, no se usa la palabra "uno" al
+        // principio. e.g 1001 => mil uno, no uno mil
+        return (n < 2000 ? "" : findNameForNumber(n / 1000)) + " mil " + unidad;
+    }
+    return "Número fuera de rango";
 }
 
-const string findNameForNumber(long n) {
-  if (n == 0)
-    return "cero";
-  if (n == 20)
-    return "veinte";
-  if (n == 100)
-    return "cien";
-  if (n == 1000)
-    return "mil";
-
-  if (n < 10) {
-    // cubre del 1 al 10
-    return base[n];
-  } else if (n < 20) {
-    // cubre del 10 al 19
-    return especiales[n - 10];
-  } else if (n < 100) {
-    // cubre del 20 al 99
-    string separador = (n % 10 == 0 || n < 30) ? "" : " y ";
-    return decenas[n / 10] + separador + base[n % 10];
-  } else if (n < 1000) {
-    // cubre del 100 al 999
-    string decena = (n % 100 == 0 ? "" : findNameForNumber(n % 100));
-    return centenas[n / 100] + ' ' + decena;
-  } else if (n < LIMIT) {
-    string unidad = (n % 1000 == 0 ? "" : findNameForNumber(n % 1000));
-    // para los numeros entre 1000 y 1999, no se usa la palabra "uno" al
-    // principio. e.g 1001 => mil uno
-    return (n < 2000 ? "" : findNameForNumber(n / 1000)) + " mil " + unidad;
-  }
-  return "Número fuera de rango";
+double readInput() {
+    double input;
+    printf("Entrada: ");
+    while (!(cin >> input) || input < 0 || input > LIMIT) {
+        cin.clear();
+        cin.ignore(1000, '\n');
+        printf("\nIngrese un número entre 0 y %.2f: ", LIMIT - 0.01);
+    }
+    return input;
 }
 
 int main(void) {
+    printf("Bienvenido al programa un número a palabra\n");
 
-  printf("Ingrese un número entero: ");
-  return 0;
+    while (true) {
+        printf("Ingrese un número entre 0 y %.2f: \n", LIMIT - 0.01);
+
+        double input = readInput();
+        double decimal = (input - floor(input)) * 100;
+
+        string output = findNameForNumber((int)input);
+        output[0] = toupper(output[0]);
+
+        if (decimal == 0.0) {
+            cout << " * " << output << endl;
+        } else {
+            cout << " * " << output << " con " << decimal << " centavos" << endl;
+        }
+    }
+    return 0;
 }
