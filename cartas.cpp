b7feb6793f47cbea654aecb0f55f21d2aa1b0560
DanikingRD <danikingrd@gmail.com>
2023-12-13 13:52:12 -0400

    cartas

--- cartas.cpp
+++ cartas.cpp
@@ -8,7 +8,7 @@
  *              1115808 - Edward Diaz
  *              1113902 - Yu jhih Chen Yeh
  *              1116238 - I Chia Chen Wang
- * FECHA: 11/11/2023 <== Fecha de realización
+ * FECHA: 12/12/2023 <== Fecha de realización
  */
 
 #include <iostream>
@@ -23,69 +23,71 @@ struct Card {
     int number;
 };
 
-Card cartas[MAX];
+Card cards[MAX];
+int readInt();
 
 void poblarCartas() {
     int i = 0;
     // asignar cada simbolo a 13 cartas
     for (int j = 0; j < 4; j++) {
         for (int k = 1; k <= 13; k++) {
-            cartas[i].symbol = symbols[j];
-            cartas[i].number = k;
+            cards[i].symbol = symbols[j];
+            cards[i].number = k;
             i++;
         }
     }
 }
 
-void printCardArray() {
+void printCardArray(Card* arr) {
     // imprimir las 52 unidades, didividas en 4 grupos de 13
     for (int i = 0; i < MAX; i++) {
-        cout << cartas[i].symbol << cartas[i].number << " ";
+        cout << arr[i].symbol << arr[i].number << " ";
         if ((i + 1) % 13 == 0) {
             cout << endl;
         }
     }
 }
 
-bool containsCard(Card* arr, int arrSize, Card card) {
-    for (int i = 0; i < arrSize; i++) {
-        if (arr[i].symbol == card.symbol && arr[i].number == card.number) {
+bool arrayContains(int* arr, int size, int n) {
+    for (int i = 0; i < 6; i++) {
+        if (arr[i] == n) {
             return true;
         }
     }
     return false;
 }
 
-bool containsValue(int* arr, int n, int value) {
-    for (int i = 0; i < n; i++) {
-        if (arr[i] == value) {
-            return true;
-        }
-    }
-    return false;
-}
-
-void shuffle(Card* cards, int n) {
-
-    Card output[MAX];
-    int aux[n];
+void shuffle(Card* arr) {
+    Card output[MAX] = {};
+    bool inserted[MAX] = {};
+    int aux[6] = {};
 
     for (int i = 0; i < MAX; i++) {
-        Card randomCard = cards[rand() % MAX];
-        while (containsCard(output, i, randomCard) || containsValue(aux, n, randomCard.number)) {
-            randomCard = cards[rand() % MAX];
-        }
+        int j;
+        Card randomCard;
+        do {
+            j = rand() % MAX;
+            randomCard = cards[j];
+        } while (arrayContains(aux, 6, randomCard.number) || inserted[j]);
 
         output[i] = randomCard;
-        aux[i % n] = randomCard.number;
+        inserted[j] = true;
+        aux[i % 6] = randomCard.number;
     }
 
     for (int i = 0; i < MAX; i++) {
-        cards[i] = output[i];
+        arr[i] = output[i];
     }
 }
+void makeShuffle() {
+    cout << endl << "Cartas antes de barajar:" << endl;
+    printCardArray(cards);
+    shuffle(cards);
+    cout << endl << "Cartas despues de barajar:" << endl;
+    printCardArray(cards);
+}
 
-int getInt() {
+int readInt() {
     int n;
     while (!(cin >> n)) {
         cout << "Intente de nuevo: " << endl;
@@ -93,21 +95,6 @@ int getInt() {
         cin.ignore(123, '\n');
     }
     return n;
-}
-
-void makeShuffle() {
-    cout << "Ingrese la distancia de separacion: ";
-    int n = getInt();
-    while (n < 0) {
-        cout << "La distancia de separacion debe ser mayor o igual a 6" << endl;
-        cout << "Ingrese la distancia de separacion: ";
-        n = getInt();
-    }
-    cout << endl << "Cartas antes de barajar:" << endl;
-    printCardArray();
-    shuffle(cartas, n);
-    cout << endl << "Cartas despues de barajar:" << endl;
-    printCardArray();
 }
 
 int main() {
@@ -122,13 +109,13 @@ int main() {
         cout << "1. Para mostrar las cartas." << endl;
         cout << "2. Para barajar las cartas." << endl;
         cout << "Opcion: ";
-        int option = getInt();
+        int option = readInt();
         switch (option) {
         case 0:
             quit = true;
             break;
         case 1:
-            printCardArray();
+            printCardArray(cards);
             break;
         case 2:
             makeShuffle();
