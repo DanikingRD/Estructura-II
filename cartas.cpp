0d7d04aa586ded24f2c46e5d367f30f1f29793ce
DanikingRD <danikingrd@gmail.com>
2023-12-13 22:55:51 -0400

    triangulo done

--- cartas.cpp
+++ cartas.cpp
@@ -65,10 +65,21 @@ void shuffle(Card* arr) {
     for (int i = 0; i < MAX; i++) {
         int j;
         Card randomCard;
-        do {
+        // do {
+        //     j = rand() % MAX;
+        //     randomCard = cards[j];
+        //     if (i == 50) {
+        //         break;
+        //     }
+        // } while (arrayContains(aux, 6, randomCard.number) || inserted[j]);
+
+        while (true) {
             j = rand() % MAX;
             randomCard = cards[j];
-        } while (arrayContains(aux, 6, randomCard.number) || inserted[j]);
+            if (!arrayContains(aux, 6, randomCard.number) && !inserted[j]) {
+                break;
+            }
+        }
 
         output[i] = randomCard;
         inserted[j] = true;
--- triangulo/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# always run a clean build
-rm -f triangulo
-g++ -std=c++17 -o triangulo triangulo.cpp -lSDL2
-./triangulo
